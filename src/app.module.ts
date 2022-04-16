75debe24c792cf16d9ab804d4d3f1141c27f031c
Zhu Aisi <all_but_last@163.com>
2022-04-16 20:53:16 +0800

    Add Sqlite database support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+let database_path = process.env.DOMELAND_DATABASE;
+
+if (!database_path) {
+  console.error('Environment variable "DOMELAND_DATABASE" is not set.');
+  process.exit(1);
+}
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: database_path,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
